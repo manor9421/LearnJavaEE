27fb6011d06a96d199a7bc054921f8bb59ab877b
manor9421 <manor9421@yahoo.co.jp>
2016-10-01 11:47:35 +0300

    +tomcat_start

---
+++ LearnTomcat/src/com/mnr/ServletMVC.java
@@ -0,0 +1,45 @@
+package com.mnr;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet(name = "/Servlet", urlPatterns = "/post")// наши переменные
+public class ServletMVC extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletMVC() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//request.setAttribute("posts", GuestBook.getAllPosts());// результат выполнения готовим к отправке
+		request.getRequestDispatcher("WEB_INF/posts.jsp");//куда отправляем
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
