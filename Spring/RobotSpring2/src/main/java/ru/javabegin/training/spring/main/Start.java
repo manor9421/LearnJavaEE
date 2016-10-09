faa2a116d71fbbe4c45ba8c1792180eec5a4abb5
manor9421 <manor9421@yahoo.co.jp>
2016-10-09 13:49:38 +0300

    add more

--- Spring/RobotSpring2/src/main/java/ru/javabegin/training/spring/main/Start.java
+++ Spring/RobotSpring2/src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -5,12 +5,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ru.javabegin.training.spring.impls.robot.ModelT1000;
+import ru.javabegin.training.spring.interfaces.Robot;
+import ru.javabegin.training.spring.interfaces.RobotConveyor;
 
 public class Start {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		
-		Object obj = context.getBean("t1000");
+		/*Object obj = context.getBean("t1000");
 		
 		if(obj instanceof ModelT1000){
 			ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
@@ -20,9 +22,19 @@ public class Start {
 			
 			t1000 = (ModelT1000) context.getBean("t1000");
 			System.out.println(t1000);
+		}	*/
+			
+			RobotConveyor t1000Conveyor = (RobotConveyor) context.getBean("t1000Conveyor");
+
+			Robot terminator1 = t1000Conveyor.createRobot();
+			Robot terminator2 = t1000Conveyor.createRobot();
+			Robot terminator3 = t1000Conveyor.createRobot();
+
+			System.out.println("terminator1 " + terminator1);
+			System.out.println("terminator2 " + terminator2);
+			System.out.println("terminator3 " + terminator3);
 			
 			
-			
-		}
+		
 	}
 }
