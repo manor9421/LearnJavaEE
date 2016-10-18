893d3dae97b44580c3edd7087ded47c7a1268e82
manor9421 <manor9421@yahoo.co.jp>
2016-10-18 21:06:34 +0300

    add more code

--- Hibernate/hibernate-course/src/main/java/com/infiniteskills/data/entities/Investment.java
+++ Hibernate/hibernate-course/src/main/java/com/infiniteskills/data/entities/Investment.java
@@ -2,12 +2,36 @@ package com.infiniteskills.data.entities;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.TableGenerator;
 
-@MappedSuperclass// наследник будет иметь все эти методы и поля. Имеет смысл, если у таблиц некоторые одинаковые колонки
+//@MappedSuperclass// наследник будет иметь все эти методы и поля. Имеет смысл, если у таблиц некоторые одинаковые колонки
+@Entity// #jpa
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)// #jpa
 public abstract class Investment {
 
+	@Id// #jpa
+	@GeneratedValue(strategy=GenerationType.TABLE,generator="key_generator")// #jpa
+	@TableGenerator(table="ifinances_keys",pkColumnName="pk_name",
+		valueColumnName="pk_value",name="key_generator")// #jpa
+	@Column(name="INVESTMENT_ID")
+	private Long investmentId;// #jpa
+	
+	@JoinColumn(name="PORTFOLIO_ID")
+	@ManyToOne(cascade=CascadeType.ALL)
+	private Portfolio portfolio;
+	
+	
 	@Column(name = "NAME")
 	protected String name;
 
