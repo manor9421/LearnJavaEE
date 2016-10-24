c2ee1d860115db2366a3249b66743953491fec12
manor9421 <manor9421@yahoo.co.jp>
2016-10-24 12:21:10 +0300

    add more

---
+++ Struts2/ProteinTracker/src/com/mnr/struts/actions/EnterProtein.java
@@ -0,0 +1,14 @@
+package com.mnr.struts.actions;
+
+import com.opensymphony.xwork2.Action;
+
+public class EnterProtein implements Action{
+
+	@Override
+	public String execute() throws Exception {
+		
+		
+		
+		return SUCCESS;
+	}
+}
